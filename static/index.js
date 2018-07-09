51e39c25a8cd8100195a1e7c83ef0bc5766578f6
Mat <mat.frayne@gmail.com>
2018-07-09 22:27:58 +0930

    .

--- static/index.js
+++ static/index.js
@@ -1,8 +1,3 @@
-hljs.configure({
-    tabReplace: '    ', // 4 spaces
-    classPrefix: '' // don't append class prefix
-        // … other options aren't changed
-})
 cart = {
     "type": null,
     "items": {}
@@ -17,8 +12,8 @@ function getRegexes(lang) {
     return {
         "STARTEND": new RegExp("(?<=[" + comment + "\/\/]\\s&&START&&\\s)(.|\\n|\\r)*?([" + comment + "\/\/]\\s&&END&&)", "g"),
         "NAME": new RegExp("(?<=[" + comment + "\/\/]\\s&&NAME&&\\s)(.|\\n|\\r)*?(?=\\n)", "g"),
-        "INPUT": new RegExp("(?<=\\s&&INPUT&&\\s)([\\s\\S]*)(?=\\n" + comment + ")", "gm"),
-        "OUTPUT": new RegExp("(?<=" + comment + "\\s&&OUTPUT&&\\s)([\\s\\S]*)(?=\\n" + comment + ")", "gm"),
+        "INPUT": new RegExp("(?<=\\s&&INPUT&&\\s)([\\s\\S]*?)(?=\\n" + comment + ")", "gm"),
+        "OUTPUT": new RegExp("(?<=\\s&&OUTPUT&&\\s)([\\s\\S]*?)(?=\\n" + comment + ")", "gm"),
         "CODE": new RegExp("^(?![" + comment + "\/\/]).+", "gm"),
         "credit": new RegExp("(?<=&&CREDIT&&\\s)(.*)", "g"),
         "tags": new RegExp("(?<=&&TAGS&&\\s)(.*)", "g")
@@ -33,33 +28,35 @@ $(document).on("click", ".dropdown button", function () {
     $(this).parent().css("height", ($(".dropdown-content").is(":visible") ? "100%" : "20px"))
 });
 $(document).on("click", ".dropdown-content a", function () {
-    $(".dropbtn").text($(this).text())
-    $(".theme").remove();
-    $("head").append('<link rel="stylesheet" class="theme" href="static/styles/' + $(this).text() + '.css">')
-    $(".dropdown-content a").css("background-color", "#f1f1f1")
-    $(".dropdown-content a").css("color", "black")
-    $(this).css("color", "white")
-    $(this).css("background-color", "#383838");
     scroll = $(window).scrollTop();
-    setTimeout(function () { $('html, body').scrollTop(scroll) }, 1);
+    console.log(scroll)
+    that = this
+    $("#container").fadeOut(0, function () {
+        $(".dropbtn").text($(that).text())
+        $(".theme").remove();
+        $("head").append('<link rel="stylesheet" class="theme" href="static/styles/' + $(that).text() + '.css">').ready(function () {
+            $('html, body').scrollTop(scroll, $("#container").fadeIn(0, function () {
+                $(".dropdown-content a").css("background-color", "#f1f1f1")
+                $(".dropdown-content a").css("color", "black")
+                $(that).css("color", "white")
+                $(that).css("background-color", "#383838");
+            }))
+        })
+    });
 });
 $(function () {
     $.each(themes, function (_, theme) {
         $(".dropdown-content").append("<a href='#'>" + theme + "</a>")
     })
     $.get("https://api.github.com/repos/Mat-Frayne/MySnippets/contents/snippets", function (data) {
-            for (x in data) {
-                df = data[x].name.match(/(.*)(?=\.)/)[0]
-                $("#contents").append($("<div/>", { class: "langClick", text: df }))
-                $.get(data[x].download_url, function (out) {
-                    parse(out, df.toLowerCase());
-                })
-            }
-        })
-        // language = "python"
-        // var response2 = $.get("snippets/python.py", function (data) {
-        //     parse(data, language);
-        // });
+        for (x in data) {
+            df = data[x].name.match(/(.*)(?=\.)/)[0]
+            $("#contents").append($("<div/>", { class: "langClick", text: df, "data-link": data[x].name }))
+            $.get(data[x].download_url, function (out) {
+                parse(out, df.toLowerCase());
+            })
+        }
+    })
 });
 
 function parse(out, lang) {
@@ -73,8 +70,8 @@ function parse(out, lang) {
         output = snip.match(regexs["OUTPUT"]);
         source = snip.match(regexs["SOURCE"])
         if (name != "null") $("#mainContent").append("<div class='blockTitle'>" + name.trim() + ":</div>")
-        if (code != "null") $("#mainContent").append(`Code:<pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
-        if (input != "null") $("#mainContent").append(`Input:\n<pre><code class='` + lang + `'>` + escapeHtml(input.join("\n")) + `</code></pre>`)
+        if (code != "null") $("#mainContent").append(`Code: <div class='copy'></div><pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
+        if (input != "null") $("#mainContent").append(`Input:<div class='copy'></div>\n<pre><code class='` + lang + `'>` + escapeHtml(input.join("\n")) + `</code></pre>`)
         if (output != "null") $("#mainContent").append(`Output:\n<pre><code class='` + lang + `'>` + escapeHtml(output.join("\n")) + `</code></pre>`)
     }
     $('code').each(function (i, block) {
