3559fd623f1eb5cdd7aca880273dfca3847645a3
Mat <mat.frayne@gmail.com>
2018-07-09 21:29:15 +0930

    .

---
+++ snippets/javascript.js
@@ -0,0 +1,14 @@
+//&&START&&
+function escapeHtml(text) {
+    return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+//&&NAME&& Escape Html
+//&&INPUT&& console.log(escapeHtml("<div id='test'></div>"));
+//&&OUTPUT&& "&lt;div id=&#039;test&#039;&gt;&lt;/div&gt;"
+//&&END&&
+//
+//&&START&&
+//&&NAME&&
+//&&INPUT&&
+//&&OUTPUT&&
+//&&END&&
--- static/index.js
+++ static/index.js
@@ -1,3 +1,8 @@
+hljs.configure({
+    tabReplace: '    ', // 4 spaces
+    classPrefix: '' // don't append class prefix
+        // … other options aren't changed
+})
 cart = {
     "type": null,
     "items": {}
@@ -6,31 +11,27 @@ comments = {
     "python": "#",
     "javascript": "//"
 }
-regexs = {
-    "STARTEND": /(?<=[#\/\/]\s&&START&&\s)(.|\n|\r)*?(?=[#\/\/]\s&&END&&)/g,
-    "NAME": /(?<=[#\/\/]\s&&NAME&&\s)(.|\n|\r)*?(?=\n)/g,
-    "INPUT": /(?<=\s&&INPUT&&\s)(.*\n*)*?(?=["""#])/g,
-    "OUTPUT": /(?<=\s&&OUTPUT&&\s)(.*\n*)*?(?=["""#])/g,
-    "CODE": /^(?![#\/\/]).+/gm,
-    "credit": /(?<=&&CREDIT&&\s)(.*)/g,
-    "tags": /(?<=&&TAGS&&\s)(.*)/g
+
+function getRegexes(lang) {
+    comment = comments[lang];
+    return {
+        "STARTEND": new RegExp("(?<=[" + comment + "\/\/]\\s&&START&&\\s)(.|\\n|\\r)*?([" + comment + "\/\/]\\s&&END&&)", "g"),
+        "NAME": new RegExp("(?<=[" + comment + "\/\/]\\s&&NAME&&\\s)(.|\\n|\\r)*?(?=\\n)", "g"),
+        "INPUT": new RegExp("(?<=\\s&&INPUT&&\\s)([\\s\\S]*)(?=\\n" + comment + ")", "gm"),
+        "OUTPUT": new RegExp("(?<=" + comment + "\\s&&OUTPUT&&\\s)([\\s\\S]*)(?=\\n" + comment + ")", "gm"),
+        "CODE": new RegExp("^(?![" + comment + "\/\/]).+", "gm"),
+        "credit": new RegExp("(?<=&&CREDIT&&\\s)(.*)", "g"),
+        "tags": new RegExp("(?<=&&TAGS&&\\s)(.*)", "g")
+    }
 }
 
 function escapeHtml(text) {
     return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 }
-$(function () {
-    // $.each(snippets, function (key, snip) {
-    //     $("#contents").append("<a data-snip='" + key + "' class='langClick' href='#" + key + "'>" + key + "</a>")
-    // })
-    $.each(themes, function (_, theme) {
-        $(".dropdown-content").append("<a href='#'>" + theme + "</a>")
-    })
-});
 $(document).on("click", ".dropdown button", function () {
     $(".dropdown-content").toggle()
     $(this).parent().css("height", ($(".dropdown-content").is(":visible") ? "100%" : "20px"))
-})
+});
 $(document).on("click", ".dropdown-content a", function () {
     $(".dropbtn").text($(this).text())
     $(".theme").remove();
@@ -43,38 +44,38 @@ $(document).on("click", ".dropdown-content a", function () {
     setTimeout(function () { $('html, body').scrollTop(scroll) }, 1);
 });
 $(function () {
-    $.get("https://api.github.com/repos/Mat-Frayne/MySnippets/contents/snippets", function (data) {
-        for (x in data) {
-            console.log(data[x])
-            console.log(data[x].name)
-            df = data[x].name.match(/(.*)(?=\.)/)[0]
-            $("#contents").append($("<div/>", { class: "langClick", text: df }))
-            $.get(data[x].download_url, function (out) {
-                parse(out, df.toLowerCase());
-            })
-        }
+    $.each(themes, function (_, theme) {
+        $(".dropdown-content").append("<a href='#'>" + theme + "</a>")
     })
-    language = "python"
-    var response2 = $.get("snippets/python.py", function (data) {
-        parse(data, language);
-    });
+    $.get("https://api.github.com/repos/Mat-Frayne/MySnippets/contents/snippets", function (data) {
+            for (x in data) {
+                df = data[x].name.match(/(.*)(?=\.)/)[0]
+                $("#contents").append($("<div/>", { class: "langClick", text: df }))
+                $.get(data[x].download_url, function (out) {
+                    parse(out, df.toLowerCase());
+                })
+            }
+        })
+        // language = "python"
+        // var response2 = $.get("snippets/python.py", function (data) {
+        //     parse(data, language);
+        // });
 });
 
 function parse(out, lang) {
-    console.log(out)
+    regexs = getRegexes(lang)
     var snippets = out.match(regexs["STARTEND"])
     for (index in snippets) {
         snip = snippets[index]
         name = snip.match(regexs["NAME"])
-        code = snip.match(regexs["CODE"])
+        code = snip.match(regexs["CODE"]);
         input = snip.match(regexs["INPUT"]);
         output = snip.match(regexs["OUTPUT"]);
         source = snip.match(regexs["SOURCE"])
         if (name != "null") $("#mainContent").append("<div class='blockTitle'>" + name.trim() + ":</div>")
-        console.log(code)
-        if (code != "null") $("#mainContent").append(`<pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
-        if (input != "null") $("#mainContent").append(`Input:\n<pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
-        if (input != "null") $("#mainContent").append(`Input:\n<pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
+        if (code != "null") $("#mainContent").append(`Code:<pre><code class='` + lang + `'>` + escapeHtml(code.join("\n")) + `</code></pre>`)
+        if (input != "null") $("#mainContent").append(`Input:\n<pre><code class='` + lang + `'>` + escapeHtml(input.join("\n")) + `</code></pre>`)
+        if (output != "null") $("#mainContent").append(`Output:\n<pre><code class='` + lang + `'>` + escapeHtml(output.join("\n")) + `</code></pre>`)
     }
     $('code').each(function (i, block) {
         hljs.highlightBlock(block);
