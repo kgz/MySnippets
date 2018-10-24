f9ffaafd829a03cf135a61adaacccb189cb62b27
Mat <mat.frayne@gmail.com>
2018-10-24 16:42:06 +1030

    added snippets

--- snippets/javascript.js
+++ snippets/javascript.js
@@ -11,3 +11,24 @@ function escapeHtml(text) {
 //&&INPUT&& console.log(escapeHtml("<div id='test'></div>"));
 //&&OUTPUT&& "&lt;div id=&#039;test&#039;&gt;&lt;/div&gt;"
 //&&END&&
+
+
+//&&START&&
+function toCommas(value) {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+//&&NAME&& Int to comma
+//&&INPUT&& toCommas(190000)
+//&&OUTPUT&& 190,000
+//&&END&&
+
+//&&START&&
+function Template() {
+    return null
+}
+
+//&&NAME&& Template
+//&&INPUT&& return null;
+//&&OUTPUT&& null
+//&&END&&
